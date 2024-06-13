9024beed1c1d4911c2c65996d809494af3a5efb0
Artv1d <154266855+Artv1d@users.noreply.github.com>
2024-06-13 17:13:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,5 @@
-def add(x: float, y: float) -> float:
-    return x + y
+def back_to_head(n: int) -> int:
+    n_str = str(n)
+    reversed_str = n_str[::-1]
+    reversed_n = int(reversed_str)
+    return reversed_n
