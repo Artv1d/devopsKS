85240a1aa910bf8d0ba762b6548e8ba27594e5a8
Artv1d <154266855+Artv1d@users.noreply.github.com>
2024-06-13 16:54:35 +0300

    Update main_tests.py

--- main_tests.py
+++ main_tests.py
@@ -7,9 +7,6 @@ def test_add():
     assert back_to_head(0) == 0
     assert back_to_head(11111) ==11111
     assert back_to_head(89) == 98
-    with pytest.raises(Zeroback_to_headError):
-        back_to_head(1, 0)
-
 
 if __name__ == "__main__":
     pytest.main()
