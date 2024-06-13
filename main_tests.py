fe2f1bfba1b83c9ccdd6bc1d3ab3eaadaae96589
Artv1d <154266855+Artv1d@users.noreply.github.com>
2024-06-13 16:57:15 +0300

    Update main_tests.py

--- main_tests.py
+++ main_tests.py
@@ -3,10 +3,10 @@ from main import back_to_head
 
 
 def test_back_to_head():
-    assert back_to_head(23) == 32
-    assert back_to_head(0) == 0
-    assert back_to_head(11111) == 11111
-    assert back_to_head(89) == 98
+    assert back_to_head(23) == 24
+    assert back_to_head(0) == 1
+    assert back_to_head(11111) == 11112
+    assert back_to_head(89) == 90
 
 if __name__ == "__main__":
     pytest.main()
